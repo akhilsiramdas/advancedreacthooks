ce50f58c645f2921ed4ff232d45140fad09ccb4f
akhilsiramdas <akhils7997@gmail.com>
2022-10-06 19:48:56 +0530

    usereducer with diff counter

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,26 +3,48 @@
 
 import * as React from 'react'
 
+// function countReducer(state, action) {
+//   return {
+//     ...state,
+//     ...(typeof action === 'function' ? action(state) : action)
+//   }
+// }
+
 function countReducer(state, action) {
-  return {
-    ...state,
-    ...(typeof action === 'function' ? action(state) : action)
+  switch (action.type) {
+    case 'INCREMENT': {
+      return { ...state,count: state.count + action.step}
+    }
+    case 'DECREMENT': {
+      return {...state,countDiff: state.countDiff - action.stepdiff}
+    }
+    default:
+      throw new Error(`Unspported action type: ${action.type}`)
   }
 }
 
-function Counter({initialCount = 0, step = 1}) {
+function Counter({initialCount = 0, initialCountdiff = 0, step = 1, stepdiff = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [state, setState] = React.useReducer(countReducer, {
-    count: initialCount
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+    countDiff : initialCountdiff
   })
-  const {count} = state
+  const {count, countDiff} = state
+  console.log('state ',state);
+  const increment = () => dispatch({type: 'INCREMENT', step})
+  const decrement = () => dispatch({type: 'DECREMENT', stepdiff})
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setState(currentState => ({count: currentState.count + step}))
-  return <button onClick={increment}>{count}</button>
+  // const increment = () => setState(currentState => ({count: currentState.count + step}))
+  return (
+    <>
+      <button onClick={increment}>{count}</button>
+      <button onClick={decrement}>{countDiff}</button>
+    </>
+  )
 }
 
 function App() {
